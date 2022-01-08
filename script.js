71c31f56a52ed470f91aa4c1e4128dd24b1e5145
Dakota Dunn <dakotadunn5@gmail.com>
2022-01-08 15:36:15 -0800

    updated javascript

---
+++ script.js
@@ -0,0 +1,71 @@
+// Assignment code here
+var finalPass = [];
+var nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var specialChars = Array.from(" !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~");
+var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var passLen;
+var promptFunc = function(){
+  var passLen = prompt("How long would you like your password? Pick a value between 8 and 128");
+  if(passLen < 8 || passLen > 128){
+    alert("You're password has to be between 8 and 128");
+  }else{
+    alert("You chose " + passLen);
+  }
+  
+  var uppCase = window.confirm("Do you want to use uppercase letters?");
+  if(uppCase){
+    finalPass = finalPass.concat(upperLetters);
+    console.log(`finalPassword: ${finalPass}`);
+  }else{
+    alert(`You don't want any uppercase letters in your password.`);
+  }
+
+  var lowerCase = window.confirm(`Do you want to use lowercase letters?`);
+  if(lowerCase){
+    finalPass = finalPass.concat(lowerLetters);
+    console.log(`finalPassword: ${finalPass}`);
+  }else{
+    alert(`You don't want any lowercase letters in your password`);
+  }
+
+  var numbers = window.confirm(`Do you want to use numbers?`);
+  if(numbers){
+    finalPass = finalPass.concat(nums);
+    console.log(`finalPass: ${finalPass}`);
+  }else{
+    alert(`You don't want numbers in your password`);
+  }
+
+  var special = window.confirm(`Do you want to use special characters?`)
+  if(special){
+    finalPass = finalPass.concat(specialChars);
+    console.log(`finalPass: ${finalPass}`);
+  }else{
+    alert(`You don't want to use special characters.`);
+  }
+
+}
+
+function randomizer(){
+  for(i = 0; i < passLen; i++){
+    Math.random(finalPass.toString([i]) * finalPass.length);
+  }
+  console.log(finalPass);
+}
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", promptFunc);
+generateBtn.addEventListener("click", randomizer);
